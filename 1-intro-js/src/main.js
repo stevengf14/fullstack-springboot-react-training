dd093a19a49982f1e16fb1350a116f14cc209293
Steven <stevengf14@gmail.com>
2023-07-03 22:07:12 -0500

    Variables and Constants

---
+++ 1-intro-js/src/main.js
@@ -0,0 +1,13 @@
+let name = "Pepe";
+name = "Andres";
+
+const lastName = "Doe";
+
+const condition = true;
+
+if (condition) {
+  const lastName = "Ale";
+  console.log(lastName);
+}
+
+console.log("Hello world!!! " + name + " " + lastName);
