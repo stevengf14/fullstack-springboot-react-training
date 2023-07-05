89a48238c2f8b4230b3539c8ac1ba964638f0aa1
Steven <stevengf14@gmail.com>
2023-07-05 00:20:33 -0500

    relationship objects and methods

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -2,11 +2,22 @@ const invoice = {
   id: 10,
   name: "office supplies",
   date: new Date(),
-  client: "John Doe",
+  client: {
+    id: 2,
+    name: "John",
+    lastName: "Doe",
+    age: 20,
+  },
   total: 1000,
+  greeting: function () {
+    return `Hello ${this.client.name}`;
+  },
 };
 
-invoice.client = "Pepe Roe";
+invoice.client.name = "Pepe";
 invoice.total = 5000;
 console.log(invoice);
 console.log(invoice.client);
+
+const greeting = invoice.greeting();
+console.log(greeting);
