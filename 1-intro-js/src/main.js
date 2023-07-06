e857985ecf05710049518845cd2a4a7a2a0d8ed3
Steven <stevengf14@gmail.com>
2023-07-05 21:35:40 -0500

    array attributes

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -8,16 +8,40 @@ const invoice = {
     lastName: "Doe",
     age: 20,
   },
-  total: 1000,
+  items: [
+    {
+      product: "keyboard",
+      price: 39,
+      quantity: 2,
+    },
+    {
+      product: "mouse",
+      price: 20,
+      quantity: 10,
+    },
+    {
+      product: "paper",
+      price: 10,
+      quantity: 10,
+    },
+  ],
+  total: function () {
+    let total = 0;
+    this.items.forEach((item) => {
+      total = total + item.price * item.quantity;
+    });
+    return total;
+  },
   greeting: function () {
     return `Hello ${this.client.name}`;
   },
 };
 
-invoice.client.name = "Pepe";
-invoice.total = 5000;
+//invoice.client.name = "Pepe";
+//invoice.total = 5000;
 console.log(invoice);
 console.log(invoice.client);
 
 const greeting = invoice.greeting();
 console.log(greeting);
+console.log("Total: " + invoice.total());
