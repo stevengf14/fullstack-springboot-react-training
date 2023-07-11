4f0f5b648ad73fd3bed4d25ddefadea908ea46fe
Steven <stevengf14@gmail.com>
2023-07-10 22:15:14 -0500

    fetch - api rest

---
+++ 1-intro-js/src/14-promises.js
@@ -0,0 +1,3 @@
+import { findInvoiceById } from "./data/invoices";
+
+findInvoiceById(3).then(console.log).catch(console.error);
--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,3 +1,12 @@
-import { findInvoiceById } from "./data/invoices";
+const httpClient = fetch("https://jsonplaceholder.typicode.com/users");
 
-findInvoiceById(3).then(console.log).catch(console.error);
+/*httpClient.then((response) => {
+  //console.log(response);
+  response.json().then(console.log);
+});*/
+
+httpClient
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+console.log("Hello world");
