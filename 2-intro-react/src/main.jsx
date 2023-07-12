e26944892a65faaab471e660180458a2bafb8041
Steven <stevengf14@gmail.com>
2023-07-11 22:52:23 -0500

    DOM in React

--- 2-intro-react/src/main.jsx
+++ 2-intro-react/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// const h1 = React.createElement('div', null, React.createElement('ul', null, React.createElement('li', null, 'item 1')));
+// const h1 = (<div><ul><li>Hello World</li></ul></div>);
+const ul = (
+  <div>
+    <h1>Hello World</h1>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  ul
+);
