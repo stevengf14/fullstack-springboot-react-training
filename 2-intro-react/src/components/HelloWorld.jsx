1030b7ed44028593416dbe998b671e980e6a567d
Steven <stevengf14@gmail.com>
2023-07-12 21:52:53 -0500

    Default Props

--- 2-intro-react/src/components/HelloWorld.jsx
+++ 2-intro-react/src/components/HelloWorld.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-export const HelloWorld = ({ user, id, title }) => {
+export const HelloWorld = ({ user, id, title, book }) => {
   console.log(title);
   return (
     <>
@@ -8,11 +8,19 @@ export const HelloWorld = ({ user, id, title }) => {
       <div>
         Whats up {user.name} {user.lastName}! Your id is {id}
       </div>
+      <div>{book}</div>
     </>
   );
 };
+
 HelloWorld.propTypes = {
   title: PropTypes.string.isRequired,
   user: PropTypes.object.isRequired,
   id: PropTypes.number.isRequired,
+  book: PropTypes.string.isRequired,
+};
+
+HelloWorld.defaultProps = {
+  title: "Hello World",
+  book: "UML",
 };
