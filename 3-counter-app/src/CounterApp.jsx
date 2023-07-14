47782db658425be72ee722e90dee8857ac61ce8b
Steven <stevengf14@gmail.com>
2023-07-13 21:29:28 -0500

    useState hook

--- 3-counter-app/src/CounterApp.jsx
+++ 3-counter-app/src/CounterApp.jsx
@@ -1,13 +1,19 @@
-export const CounterApp = () => {
-  let counter = 0;
-  const counterIncrement = () => {
-    counter = counter + 1;
-    console.log("click!", counter);
-  };
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+export const CounterApp = ({ value }) => {
+  let [counter, setCounter] = useState(value);
+
+  const counterIncrement = () => setCounter((c) => c + 1);
+
   return (
     <>
       <h2>Counter value: {counter}</h2>
       <button onClick={counterIncrement}>+</button>
     </>
   );
+};
+
+CounterApp.propTypes = {
+  value: PropTypes.number.isRequired,
 };
