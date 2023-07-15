126d0187cf078f5c456a023c1045617bbb3c722c
Steven <stevengf14@gmail.com>
2023-07-14 23:08:25 -0500

    data in component

--- 4-invoice-app/src/components/InvoiceApp.jsx
+++ 4-invoice-app/src/components/InvoiceApp.jsx
@@ -1,15 +1,36 @@
 import { getInvoice } from "../services/invoiceServices";
 
 export const InvoiceApp = () => {
-  const invoice = getInvoice();
-
+  const { id, name, client, company, items } = getInvoice();
+  const { name: nameClient, lastName, address } = client;
+  const { country, city, street, number } = address;
   return (
     <>
       <h1>Invoice Example</h1>
       <ul>
-        <li>Id: {invoice.id}</li>
-        <li>Name: {invoice.name}</li>
+        <li>Id: {id}</li>
+        <li>Name: {name}</li>
       </ul>
+
+      <h3>Client Data</h3>
+      <ul>
+        <li>
+          {nameClient} {lastName}
+        </li>
+        <li>{country}</li>
+        <li>{city}</li>
+        <li>
+          {street} {number}
+        </li>
+      </ul>
+
+      <h3>Company Data</h3>
+      <ul>
+        <li>{company.name}</li>
+        <li>{company.fiscalNumber}</li>
+      </ul>
+
+      <h4>Invoice Products</h4>
     </>
   );
 };
