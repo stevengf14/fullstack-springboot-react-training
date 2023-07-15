9440ff354c71f8e9aef151c56590dbe76ecf2227
Steven <stevengf14@gmail.com>
2023-07-14 23:00:40 -0500

    invoiceServices

--- 4-invoice-app/src/components/InvoiceApp.jsx
+++ 4-invoice-app/src/components/InvoiceApp.jsx
@@ -1,7 +1,15 @@
+import { getInvoice } from "../services/invoiceServices";
+
 export const InvoiceApp = () => {
+  const invoice = getInvoice();
+
   return (
     <>
       <h1>Invoice Example</h1>
+      <ul>
+        <li>Id: {invoice.id}</li>
+        <li>Name: {invoice.name}</li>
+      </ul>
     </>
   );
 };
