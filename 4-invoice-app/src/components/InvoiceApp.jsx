e6f2736736fef7c400b09a67ef5f0daef7e2842b
Steven <stevengf14@gmail.com>
2023-07-17 22:23:19 -0500

    css styles with bootstrap

--- 4-invoice-app/src/components/InvoiceApp.jsx
+++ 4-invoice-app/src/components/InvoiceApp.jsx
@@ -6,49 +6,63 @@ export const InvoiceApp = () => {
   const { country, city, street, number } = address;
   return (
     <>
-      <h1>Invoice Example</h1>
-      <ul>
-        <li>Id: {id}</li>
-        <li>Name: {name}</li>
-      </ul>
+      <div className="container">
+        <div className="card my-3">
+          <div className="card-header">Invoice Example</div>
 
-      <h3>Client Data</h3>
-      <ul>
-        <li>
-          {nameClient} {lastName}
-        </li>
-        <li>{country}</li>
-        <li>{city}</li>
-        <li>
-          {street} {number}
-        </li>
-      </ul>
+          <div className="card-body">
+            <ul className="list-group">
+              <li className="list-group-item">Id: {id}</li>
+              <li className="list-group-item">Name: {name}</li>
+            </ul>
 
-      <h3>Company Data</h3>
-      <ul>
-        <li>{company.name}</li>
-        <li>{company.fiscalNumber}</li>
-      </ul>
+            <div className="row my-3">
+              <div className="col">
+                <h3>Client Data</h3>
+                <ul className="list-group">
+                  <li className="list-group-item active">
+                    {nameClient} {lastName}
+                  </li>
+                  <li className="list-group-item">
+                    {country} / {city}
+                  </li>
+                  <li className="list-group-item">
+                    {street} {number}
+                  </li>
+                </ul>
+              </div>
 
-      <h4>Invoice Products</h4>
-      <table>
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>Price</th>
-            <th>Quantity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map(({ id, product, price, quantity }) => (
-            <tr key={id}>
-              <td>{product}</td>
-              <td>{price}</td>
-              <td>{quantity}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              <div className="col">
+                <h3>Company Data</h3>
+                <ul className="list-group">
+                  <li className="list-group-item active">{company.name}</li>
+                  <li className="list-group-item">{company.fiscalNumber}</li>
+                </ul>
+              </div>
+            </div>
+
+            <h4>Invoice Products</h4>
+            <table className="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                </tr>
+              </thead>
+              <tbody>
+                {items.map(({ id, product, price, quantity }) => (
+                  <tr key={id}>
+                    <td>{product}</td>
+                    <td>{price}</td>
+                    <td>{quantity}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
