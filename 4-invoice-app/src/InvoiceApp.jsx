58189707487542e61b2da6efc7e4b04afb1103b3
Steven <stevengf14@gmail.com>
2023-07-20 22:05:42 -0500

    Form component

--- 4-invoice-app/src/InvoiceApp.jsx
+++ 4-invoice-app/src/InvoiceApp.jsx
@@ -5,6 +5,7 @@ import { InvoiceView } from "./components/InvoiceView";
 import { ListItemsView } from "./components/ListItemsView";
 import { TotalView } from "./components/TotalView";
 import { useEffect, useState } from "react";
+import { FormItemsView } from "./components/FormItemsView";
 
 const invoiceInitial = {
   id: 0,
@@ -33,17 +34,9 @@ export const InvoiceApp = () => {
 
   const [invoice, setInvoice] = useState(invoiceInitial);
 
-  const { id, name, client, company } = invoice;
-
   const [items, setItems] = useState([]);
 
-  const [formItemsState, setFormItemsState] = useState({
-    product: "",
-    price: "",
-    quantity: "",
-  });
-
-  const { product, price, quantity } = formItemsState;
+  const { id, name, client, company } = invoice;
 
   useEffect(() => {
     const data = getInvoice();
@@ -55,38 +48,7 @@ export const InvoiceApp = () => {
     setTotal(calculateTotal(items));
   }, [items]);
 
-  const onInputChange = ({ target: { name, value } }) => {
-    setFormItemsState({
-      ...formItemsState,
-      [name]: value,
-    });
-  };
-
-  const onInvoiceItemsSubmit = (event) => {
-    event.preventDefault();
-
-    if (product.trim().length <= 1) {
-      alert("The product name need more than 1 character");
-      return;
-    }
-    if (price.trim().length < 1 || price <= 0) {
-      alert("The price value is not allowed");
-      return;
-    }
-    if (isNaN(price.trim())) {
-      alert("The price value is not a number");
-      return;
-    }
-    if (quantity.trim().length < 1 || quantity <= 0) {
-      alert("The quantity value is not allowed");
-      return;
-    }
-
-    if (isNaN(quantity.trim())) {
-      alert("The quantity value is not a number");
-      return;
-    }
-
+  const handlerAddItems = ({ product, price, quantity }) => {
     setItems([
       ...items,
       {
@@ -96,11 +58,7 @@ export const InvoiceApp = () => {
         quantity: parseInt(quantity.trim(), 10),
       },
     ]);
-    setFormItemsState({
-      product: "",
-      price: "",
-      quantity: "",
-    });
+
     setCounter(counter + 1);
   };
 
@@ -125,36 +83,7 @@ export const InvoiceApp = () => {
 
             <ListItemsView title="Invoice Products" items={items} />
             <TotalView total={total} />
-            <form className="w-50" onSubmit={onInvoiceItemsSubmit}>
-              <input
-                type="text"
-                name="product"
-                value={product}
-                placeholder="Product"
-                className="form-control m-3"
-                onChange={onInputChange}
-              />
-              <input
-                type="text"
-                name="price"
-                value={price}
-                placeholder="Price"
-                className="form-control m-3"
-                onChange={onInputChange}
-              />
-              <input
-                type="text"
-                name="quantity"
-                value={quantity}
-                placeholder="Quantity"
-                className="form-control m-3"
-                onChange={onInputChange}
-              />
-
-              <button type="submit" className="btn btn-primary m-3">
-                Add Item
-              </button>
-            </form>
+            <FormItemsView handler={handlerAddItems} />
           </div>
         </div>
       </div>
