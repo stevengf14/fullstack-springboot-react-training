a9c577a7d727eae92915d33734fbd0cbfc8cd7ac
Steven <stevengf14@gmail.com>
2023-07-24 21:36:52 -0500

    products data

---
+++ 5-cart-app/src/data/products.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    name: "Mechanical Keyboard RGB",
+    description: "Mechanical keyboard with RGB lights switches cherry network",
+    price: 120,
+  },
+  {
+    id: 2,
+    name: "Samsung Smart TV 55",
+    description: "Excellent 55-inch TV Samgung brand",
+    price: 1200,
+  },
+  {
+    id: 3,
+    name: "Corsair Memmory 8GB DDR5",
+    description: "RAM 8gb Caorsair brand",
+    price: 300,
+  },
+  {
+    id: 4,
+    name: "Asus Nvidia RTC",
+    description: "Asus Nvidia RTC graphics card for 4k gaming",
+    price: 750,
+  },
+  {
+    id: 5,
+    name: "CPU Intel Core i5",
+    description: "Intel i5 processor optimized for multicore tasks",
+    price: 600,
+  },
+  {
+    id: 6,
+    name: "Headphones bluetooth Sony",
+    description: "Sony Bluetooth headphones for smartphone",
+    price: 240,
+  },
+];
