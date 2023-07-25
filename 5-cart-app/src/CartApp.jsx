8f6a14cbecd38cbb8e0bd675a2bd8274af29aba5
Steven <stevengf14@gmail.com>
2023-07-24 21:21:21 -0500

    Products

--- 5-cart-app/src/CartApp.jsx
+++ 5-cart-app/src/CartApp.jsx
@@ -1,7 +1,83 @@
 export const CartApp = () => {
   return (
     <>
-      <h3>Cart App</h3>
+      <div className="container">
+        <h3>Cart App</h3>
+        <div className="row">
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Mechanical Keyboard RGB</h5>
+                <p className="card-text">
+                  Mechanical keyboard with RGB lights switches cherry network
+                </p>
+                <p className="card-text">$ 120</p>
+                <buttonc className="btn btn-primary">Add</buttonc>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Samsung Smart TV 55</h5>
+                <p className="card-text">
+                  Mechanical keyboard with RGB lights switches cherry network
+                </p>
+                <p className="card-text">$ 1200</p>
+                <buttonc className="btn btn-primary">Add</buttonc>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Corsair Memmory 8GB DDR5</h5>
+                <p className="card-text">
+                  Mechanical keyboard with RGB lights switches cherry network
+                </p>
+                <p className="card-text">$ 300</p>
+                <buttonc className="btn btn-primary">Add</buttonc>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Asus Nvidia RTC</h5>
+                <p className="card-text">
+                  Mechanical keyboard with RGB lights switches cherry network
+                </p>
+                <p className="card-text">$ 750</p>
+                <buttonc className="btn btn-primary">Add</buttonc>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">CPU Intel Core i5</h5>
+                <p className="card-text">
+                  Mechanical keyboard with RGB lights switches cherry network
+                </p>
+                <p className="card-text">$ 600</p>
+                <buttonc className="btn btn-primary">Add</buttonc>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Headphones bluetooth Sony</h5>
+                <p className="card-text">
+                  Mechanical keyboard with RGB lights switches cherry network
+                </p>
+                <p className="card-text">$ 240</p>
+                <buttonc className="btn btn-primary">Add</buttonc>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
