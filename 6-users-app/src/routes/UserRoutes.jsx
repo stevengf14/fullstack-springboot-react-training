7e24328e22bf2da2f05ee0ac8d8f71b5180681c5
Steven <stevengf14@gmail.com>
2023-08-15 23:11:44 -0500

    Context for users

--- 6-users-app/src/routes/UserRoutes.jsx
+++ 6-users-app/src/routes/UserRoutes.jsx
@@ -3,61 +3,52 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { UsersPage } from "../pages/UsersPage";
 import { Navbar } from "../components/layout/Navbar";
 import { RegistrationPage } from "../pages/RegistrationPage";
-import { useUsers } from "../hooks/useUsers";
+import { UserProvider } from "../context/UserProvider";
 
 export const UserRoutes = ({ login, handlerLogout }) => {
-  const {
-    users,
-    userSelected,
-    initialUserForm,
-    visibleForm,
-    handlerAddUser,
-    handlerRemoveUser,
-    handlerUserSelectedForm,
-    handlerOpenForm,
-    handlerCloseForm,
-  } = useUsers();
   return (
     <>
-      <Navbar login={login} handlerLogout={handlerLogout} />
-      <Routes>
-        <Route
-          path="users"
-          element={
-            <UsersPage
-              users={users}
-              userSelected={userSelected}
-              initialUserForm={initialUserForm}
-              visibleForm={visibleForm}
-              handlerAddUser={handlerAddUser}
-              handlerRemoveUser={handlerRemoveUser}
-              handlerUserSelectedForm={handlerUserSelectedForm}
-              handlerOpenForm={handlerOpenForm}
-              handlerCloseForm={handlerCloseForm}
-            />
-          }
-        />
-        <Route
-          path="users/registration"
-          element={
-            <RegistrationPage
-              handlerAddUser={handlerAddUser}
-              initialUserForm={initialUserForm}
-            />
-          }
-        />
-        <Route
-          path="users/edit/:id"
-          element={
-            <RegistrationPage
-              users={users}
-              handlerAddUser={handlerAddUser}
-              initialUserForm={initialUserForm}
-            />
-          }
-        />
-        <Route path="/" element={<Navigate to="/users" />} />
-      </Routes>
+      <UserProvider>
+        <Navbar login={login} handlerLogout={handlerLogout} />
+        <Routes>
+          <Route
+            path="users"
+            element={
+              <UsersPage
+                users={users}
+                userSelected={userSelected}
+                initialUserForm={initialUserForm}
+                visibleForm={visibleForm}
+                handlerAddUser={handlerAddUser}
+                handlerRemoveUser={handlerRemoveUser}
+                handlerUserSelectedForm={handlerUserSelectedForm}
+                handlerOpenForm={handlerOpenForm}
+                handlerCloseForm={handlerCloseForm}
+              />
+            }
+          />
+          <Route
+            path="users/registration"
+            element={
+              <RegistrationPage
+                handlerAddUser={handlerAddUser}
+                initialUserForm={initialUserForm}
+              />
+            }
+          />
+          <Route
+            path="users/edit/:id"
+            element={
+              <RegistrationPage
+                users={users}
+                handlerAddUser={handlerAddUser}
+                initialUserForm={initialUserForm}
+              />
+            }
+          />
+          <Route path="/" element={<Navigate to="/users" />} />
+        </Routes>
+      </UserProvider>
     </>
   );
 };
