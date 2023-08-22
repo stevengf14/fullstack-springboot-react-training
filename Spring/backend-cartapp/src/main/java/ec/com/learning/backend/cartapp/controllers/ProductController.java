a7d120a9f6030ac9663b7aead88a6ea4552adc7b
Steven <stevengf14@gmail.com>
2023-08-21 23:14:28 -0500

    Rest Controller

---
+++ Spring/backend-cartapp/src/main/java/ec/com/learning/backend/cartapp/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package ec.com.learning.backend.cartapp.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.com.learning.backend.cartapp.models.entities.Product;
+import ec.com.learning.backend.cartapp.services.ProductService;
+
+@RestController
+public class ProductController {
+
+	@Autowired
+	private ProductService service;
+
+	@GetMapping("/products")
+	public List<Product> list() {
+		return service.findAll();
+	}
+
+}
--- Spring/backend-cartapp/src/main/java/ec/com/learning/backend/cartapp/services/ProductServiceImpl.java
+++ Spring/backend-cartapp/src/main/java/ec/com/learning/backend/cartapp/services/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import ec.com.learning.backend.cartapp.models.entities.Product;
 import ec.com.learning.backend.cartapp.repositories.ProductRepository;
@@ -15,6 +16,7 @@ public class ProductServiceImpl implements ProductService {
 	private ProductRepository repository;
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Product> findAll() {
 		return (List<Product>) repository.findAll();
 	}
