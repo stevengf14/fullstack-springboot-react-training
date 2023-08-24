ed7820b26f6397ab60f7c9e0de941f0f287b5444
Steven <stevengf14@gmail.com>
2023-08-23 22:55:41 -0500

    api rest from backend

--- React/5-cart-app/src/services/productService.js
+++ React/5-cart-app/src/services/productService.js
@@ -1,6 +1,6 @@
-import { products } from "../data/products";
-
-export const getProducts = () => {
+export const getProducts = async () => {
+  const response = await fetch("http://localhost:8080/products");
+  const products = await response.json();
   return products;
 };
 
