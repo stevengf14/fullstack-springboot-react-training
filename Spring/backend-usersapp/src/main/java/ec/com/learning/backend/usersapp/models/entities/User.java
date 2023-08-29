2534a81898476a0ae2a4a4b41dc9c7208603c274
Steven <stevengf14@gmail.com>
2023-08-28 22:09:30 -0500

    creating backend-usersapp project

---
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/models/entities/User.java
@@ -0,0 +1,5 @@
+package ec.com.learning.backend.usersapp.models.entities;
+
+public class User {
+
+}
---
+++ Spring/backend-usersapp/src/test/java/ec/com/learning/backend/usersapp/BackendUsersappApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.learning.backend.usersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
