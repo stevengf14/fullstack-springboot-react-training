5e37e2415af195be741951c4443433427dc595b5
Steven <stevengf14@gmail.com>
2023-08-29 23:45:49 -0500

    UserController PostMapping

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/controllers/UserController.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/controllers/UserController.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import ec.com.learning.backend.usersapp.models.entities.User;
@@ -32,6 +36,18 @@ public class UserController {
 			return ResponseEntity.ok(userOptional.orElseThrow());
 		}
 		return ResponseEntity.notFound().build();
+	}
+
+	/*
+	 * @PostMapping
+	 * 
+	 * @ResponseStatus(HttpStatus.CREATED) public User create(@RequestBody User
+	 * user) { return service.save(user); }
+	 */
+
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody User user) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
 	}
 
 }
