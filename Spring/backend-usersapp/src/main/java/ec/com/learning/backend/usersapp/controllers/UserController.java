6602d4dbd5f341959fda55852919c9ad9dd56eb3
Steven <stevengf14@gmail.com>
2023-08-29 23:41:22 -0500

    UserController

---
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/controllers/UserController.java
@@ -0,0 +1,37 @@
+package ec.com.learning.backend.usersapp.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.com.learning.backend.usersapp.models.entities.User;
+import ec.com.learning.backend.usersapp.services.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public List<User> list() {
+		return service.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		Optional<User> userOptional = service.findById(id);
+		if (userOptional.isPresent()) {
+			return ResponseEntity.ok(userOptional.orElseThrow());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+}
