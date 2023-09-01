2f5cd208fa65f57cbf230b3648dfa6dded760a27
Steven <stevengf14@gmail.com>
2023-08-31 23:32:30 -0500

    API RESTFULL POST, PUT, DELETE

--- React/6-users-app/src/services/userService.js
+++ React/6-users-app/src/services/userService.js
@@ -1,10 +1,37 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:8080/users";
 export const findAll = async () => {
   try {
-    return await axios.get("http://localhost:8080/users");
+    return await axios.get(BASE_URL);
   } catch (error) {
     console.log(error);
   }
   return null;
+};
+
+export const save = async ({ username, email, password }) => {
+  try {
+    return await axios.post(BASE_URL, username, email, password);
+  } catch (error) {
+    console.error(error);
+  }
+  return undefined;
+};
+
+export const update = async ({ id, username, email }) => {
+  try {
+    return await axios.put(`${BASE_URL}/${id}`, username, email);
+  } catch (error) {
+    console.error(error);
+  }
+  return undefined;
+};
+
+export const remove = async (id) => {
+  try {
+    await axios.delete(`${BASE_URL}/${id}`);
+  } catch (error) {
+    console.error(error);
+  }
 };
