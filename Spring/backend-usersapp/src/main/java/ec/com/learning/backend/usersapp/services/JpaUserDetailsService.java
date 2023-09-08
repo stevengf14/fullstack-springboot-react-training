e9ebb2cc24ccabefc57ff9a68482aa3ed1a12485
Steven <stevengf14@gmail.com>
2023-09-07 22:06:01 -0500

    UserDetailsService

---
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/services/JpaUserDetailsService.java
@@ -0,0 +1,28 @@
+package ec.com.learning.backend.usersapp.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class JpaUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		if (!username.equals("admin")) {
+			throw new UsernameNotFoundException(String.format("Username %s does not exists", username));
+		}
+
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+		return new User(username, "12345", true, true, true, true, authorities);
+	}
+
+}
