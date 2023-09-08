4db9c42012dd4733b1ee85a24eefcce393a20e67
Steven <stevengf14@gmail.com>
2023-09-07 21:36:43 -0500

    authentication method

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/filters/JwtAuthenticationFilter.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/filters/JwtAuthenticationFilter.java
@@ -3,10 +3,16 @@ package ec.com.learning.backend.usersapp.auth.filters;
 import java.io.IOException;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ec.com.learning.backend.usersapp.models.entities.User;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -23,8 +29,26 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
 			throws AuthenticationException {
-		
-		return authenticationManager.authenticate(null);
+		User user = null;
+		String username = null;
+		String password = null;
+
+		try {
+			user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+			username = user.getUsername();
+			password = user.getPassword();
+
+			logger.info("Username from InputStream request (raw) " + username);
+			logger.info("Password from InputStream request (raw) " + password);
+		} catch (StreamReadException e) {
+			e.printStackTrace();
+		} catch (DatabindException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+		return authenticationManager.authenticate(authToken);
 	}
 
 	@Override
