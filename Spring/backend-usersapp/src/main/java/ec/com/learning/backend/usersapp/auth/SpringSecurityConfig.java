02159dbb91e286e75233845ac83273cedb7dd7b5
Steven <stevengf14@gmail.com>
2023-09-07 21:58:46 -0500

    Spring Security config

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/SpringSecurityConfig.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/SpringSecurityConfig.java
@@ -1,19 +1,28 @@
 package ec.com.learning.backend.usersapp.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import ec.com.learning.backend.usersapp.auth.filters.JwtAuthenticationFilter;
+
 @Configuration
 public class SpringSecurityConfig {
+
+	@Autowired
+	private AuthenticationConfiguration authenticationConfiguration;;
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http.authorizeHttpRequests().requestMatchers(HttpMethod.GET, "/users").permitAll().anyRequest()
-				.authenticated().and().csrf(config -> config.disable())
+				.authenticated().and()
+				.addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
+				.csrf(config -> config.disable())
 				.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.build();
 	}
