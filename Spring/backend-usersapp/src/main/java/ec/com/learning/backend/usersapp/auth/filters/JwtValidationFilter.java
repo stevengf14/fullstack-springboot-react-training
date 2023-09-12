94305e8e925658540b71e6cf68cf6d00ff8bc2dd
Steven <stevengf14@gmail.com>
2023-09-11 23:36:22 -0500

    JwtValidationFilter

---
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/filters/JwtValidationFilter.java
@@ -0,0 +1,31 @@
+package ec.com.learning.backend.usersapp.auth.filters;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtValidationFilter extends BasicAuthenticationFilter {
+
+	public JwtValidationFilter(AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String header = request.getHeader("Authorization");
+		if (header == null || !header.startsWith("Bearer ")) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+		String token = header.replace("Bearer ", "");
+	}
+
+}
