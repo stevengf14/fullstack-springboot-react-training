76fde6aae595981f9217d2ad75f072a2101342fd
Steven <stevengf14@gmail.com>
2023-09-14 00:02:33 -0500

    token validation

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/filters/JwtValidationFilter.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/filters/JwtValidationFilter.java
@@ -1,10 +1,20 @@
 package ec.com.learning.backend.usersapp.auth.filters;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -29,8 +39,25 @@ public class JwtValidationFilter extends BasicAuthenticationFilter {
 		String token = header.replace("Bearer ", "");
 		byte[] tokenDecodeBytes = Base64.getDecoder().decode(token);
 		String tokenDecode = new String(tokenDecodeBytes);
-		
+
 		String[] tokenArray = tokenDecode.split(".");
+		String secret = tokenArray[0];
+		String username = tokenArray[1];
+
+		if ("some_secret_token".equals(secret)) {
+			List<GrantedAuthority> authorities = new ArrayList<>();
+			authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+			UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username,
+					authorities);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			chain.doFilter(request, response);
+		} else {
+			Map<String, String> body = new HashMap<>();
+			body.put("message", "JWT Token is not valid");
+			response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+			response.setStatus(403);
+			response.setContentType("application/json");
+		}
 	}
 
 }
