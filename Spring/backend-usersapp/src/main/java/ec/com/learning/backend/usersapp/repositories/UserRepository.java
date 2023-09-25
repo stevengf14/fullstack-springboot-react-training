0502c21d28fca720f335f2c6aca71e26f3017f44
Steven <stevengf14@gmail.com>
2023-09-25 18:22:37 -0500

    Login with JPA

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/repositories/UserRepository.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/repositories/UserRepository.java
@@ -1,9 +1,16 @@
 package ec.com.learning.backend.usersapp.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import ec.com.learning.backend.usersapp.models.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+	Optional<User> findByUsername(String username);
+
+	@Query("select u from User u where u.username=?1")
+	Optional<User> getUserByUsername(String username);
 }
--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/services/JpaUserDetailsService.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/services/JpaUserDetailsService.java
@@ -2,7 +2,9 @@ package ec.com.learning.backend.usersapp.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -10,22 +12,30 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ec.com.learning.backend.usersapp.repositories.UserRepository;
 
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	@Autowired
+	private UserRepository repository;
 
-		if (!username.equals("admin")) {
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<ec.com.learning.backend.usersapp.models.entities.User> o = repository.findByUsername(username);
+
+		if (!o.isPresent()) {
 			throw new UsernameNotFoundException(String.format("Username %s does not exists", username));
 		}
+		ec.com.learning.backend.usersapp.models.entities.User user = o.orElseThrow();
 
 		List<GrantedAuthority> authorities = new ArrayList<>();
 		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
 
-		return new User(username, "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS", true, true, true,
-				true, authorities);
+		return new User(user.getUsername(), user.getPassword(), true, true, true, true, authorities);
 	}
 
 }
