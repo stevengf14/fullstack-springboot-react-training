4e603fc8e815254d449b9c71d78fbf629d98b63d
Steven <stevengf14@gmail.com>
2023-09-26 18:54:21 -0500

    jwt

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/TokenJwtConfig.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/TokenJwtConfig.java
@@ -1,8 +1,15 @@
 package ec.com.learning.backend.usersapp.auth;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+
 public class TokenJwtConfig {
 
-	public final static String SECRET_KEY = "some_secret_token";
+	// public final static String SECRET_KEY = "some_secret_token";
+	public final static Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 	public final static String PREFIX_TOKEN = "Bearer ";
 	public final static String HEADER_AUTHORIZATION = "Authorization";
+
+	// SecretKey key = Jwts.SIG.HS256.key().build();
 }
