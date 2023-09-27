89e9de8bd8f8b0d74272de3a346d3e195caef9a0
Steven <stevengf14@gmail.com>
2023-09-26 19:21:09 -0500

    cleaning code

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/filters/JwtValidationFilter.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/filters/JwtValidationFilter.java
@@ -2,7 +2,6 @@ package ec.com.learning.backend.usersapp.auth.filters;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
