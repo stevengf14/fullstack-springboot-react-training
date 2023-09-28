3723cd707b1d9d880bfa7cc1da799300cfcb96a3
Steven <stevengf14@gmail.com>
2023-09-27 20:20:31 -0500

    FIXING ROLES

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/models/entities/Role.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/models/entities/Role.java
@@ -11,6 +11,13 @@ import jakarta.persistence.Table;
 @Table(name = "roles")
 public class Role {
 
+	public Role() {
+	}
+
+	public Role(String name) {
+		this.name = name;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
