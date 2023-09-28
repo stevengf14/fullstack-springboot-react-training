3b7d5a0ff747c811bbfe38ab47b059e878fa4596
Steven <stevengf14@gmail.com>
2023-09-27 20:11:54 -0500

    ROLES

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/models/entities/Role.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/models/entities/Role.java
@@ -1,5 +1,6 @@
 package ec.com.learning.backend.usersapp.models.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,7 @@ public class Role {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(unique = true)
 	private String name;
 
 	public Long getId() {
--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/models/entities/User.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/models/entities/User.java
@@ -7,8 +7,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
@@ -35,6 +38,8 @@ public class User {
 	private String email;
 
 	@ManyToMany
+	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"), uniqueConstraints = {
+			@UniqueConstraint(columnNames = { "user_id", "role_id" }) })
 	private List<Role> roles;
 
 	public Long getId() {
