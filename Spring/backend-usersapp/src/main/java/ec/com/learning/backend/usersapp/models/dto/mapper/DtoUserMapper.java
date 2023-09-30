728c0dcb6bc691ba3723334e1fd698d72a8ce1c5
Steven <stevengf14@gmail.com>
2023-09-29 20:52:06 -0500

    Dto mapper builder

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/models/dto/mapper/DtoUserMapper.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/models/dto/mapper/DtoUserMapper.java
@@ -1,14 +1,31 @@
 package ec.com.learning.backend.usersapp.models.dto.mapper;
 
+import ec.com.learning.backend.usersapp.models.dto.UserDto;
+import ec.com.learning.backend.usersapp.models.entities.User;
+
 public class DtoUserMapper {
 
 	private static DtoUserMapper mapper;
+
+	private User user;
 
 	private DtoUserMapper() {
 	}
 
 	public static DtoUserMapper getInstance() {
 		return new DtoUserMapper();
+	}
+
+	public DtoUserMapper setUser(User user) {
+		this.user = user;
+		return mapper;
+	}
+
+	public UserDto build() {
+		if (user == null) {
+			throw new RuntimeException("You must use user entity!");
+		}
+		return new UserDto(user.getId(), user.getUsername(), user.getEmail());
 	}
 
 }
--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/services/UserService.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/services/UserService.java
@@ -3,18 +3,19 @@ package ec.com.learning.backend.usersapp.services;
 import java.util.List;
 import java.util.Optional;
 
+import ec.com.learning.backend.usersapp.models.dto.UserDto;
 import ec.com.learning.backend.usersapp.models.entities.User;
 import ec.com.learning.backend.usersapp.models.request.UserRequest;
 
 public interface UserService {
 
-	List<User> findAll();
+	List<UserDto> findAll();
 
-	Optional<User> findById(Long id);
+	Optional<UserDto> findById(Long id);
 
-	User save(User user);
+	UserDto save(User user);
 
-	Optional<User> update(UserRequest user, Long id);
+	Optional<UserDto> update(UserRequest user, Long id);
 
 	void remove(Long id);
 
