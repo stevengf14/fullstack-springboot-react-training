0331673b60cb1c5ee28863a576541e4239ddfeac
Steven <stevengf14@gmail.com>
2023-10-02 18:35:59 -0500

    Cors configuration Spring Security

--- Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/SpringSecurityConfig.java
+++ Spring/backend-usersapp/src/main/java/ec/com/learning/backend/usersapp/auth/SpringSecurityConfig.java
@@ -1,8 +1,12 @@
 package ec.com.learning.backend.usersapp.auth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -11,6 +15,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import ec.com.learning.backend.usersapp.auth.filters.JwtAuthenticationFilter;
 import ec.com.learning.backend.usersapp.auth.filters.JwtValidationFilter;
@@ -44,6 +52,28 @@ public class SpringSecurityConfig {
 				.addFilter(new JwtValidationFilter(authenticationConfiguration.getAuthenticationManager()))
 				.csrf(config -> config.disable())
 				.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.build();
+				.cors(cors -> cors.configurationSource(corsConfigurationSource())).build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5173"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		config.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return source;
+	}
+
+	@Bean
+	FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+				new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 }
