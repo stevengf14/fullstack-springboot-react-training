16feb4b3c6c5dcba2cf98fbe24e3f917cf22eded
Steven <stevengf14@gmail.com>
2023-10-16 23:03:01 -0500

    env variables

--- React/6-users-app/src/auth/services/authService.js
+++ React/6-users-app/src/auth/services/authService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const loginUser = async ({ username, password }) => {
   // eslint-disable-next-line no-useless-catch
   try {
-    return await axios.post("http://127.0.0.1:8080/login", {
+    return await axios.post(`${import.meta.env.VITE_API_BASE_URL}/login`, {
       username,
       password,
     });
